4b1bc3d969c7f33416d909006bb8aeb786070577
myeanji <hyean03@naver.com>
2023-07-25 13:04:33 +0900

    mia 직접입력시 input박스 추가

--- src/component/MyInfo/Container1.js
+++ src/component/MyInfo/Container1.js
@@ -109,22 +109,37 @@ const MenuItem = styled.option`
   font-weight: 400;
 `;
 
+const Writeinput = styled.input`
+  position: relative;
+  border: 1px solid #5B8E31;
+  border-radius: 20px;
+  
+  width: 188px; 
+  height: 25px;
+  top: -18.5em;
+  left: -8em;
+
+  @media all and (min-width: 1024px) {
+
+}
+`;
+
 function Selectbar() {
   return (
     <MenuBarWrapper>
     <MenuBar>
       <MenuItem value="1">----</MenuItem>
-      <MenuItem value="2">경영/사무/금융/보험직</MenuItem>
-      <MenuItem value="3">연구직 및 공학기술직</MenuItem>
-      <MenuItem value="4">교육/법률/사회복지/경찰/소방직 및 군인</MenuItem>
-      <MenuItem value="5">보건/의료직</MenuItem>
-      <MenuItem value="6">예술/디자인/방송/스포츠직</MenuItem>
-      <MenuItem value="7">미용/여행/숙박/음식/경비/청소직</MenuItem>
-      <MenuItem value="8">영업/판매/운전/운송직</MenuItem>
-      <MenuItem value="9">건설/채굴직</MenuItem>
-      <MenuItem value="10">설치/정비/생산직</MenuItem>
-      <MenuItem value="11">농림어업직 </MenuItem>
-      <MenuItem value="12">직접입력</MenuItem>
+      <MenuItem value="2">직접입력</MenuItem>
+      <MenuItem value="3">경영/사무/금융/보험직</MenuItem>
+      <MenuItem value="4">연구직 및 공학기술직</MenuItem>
+      <MenuItem value="5">교육/법률/사회복지/경찰/소방직 및 군인</MenuItem>
+      <MenuItem value="6">보건/의료직</MenuItem>
+      <MenuItem value="7">예술/디자인/방송/스포츠직</MenuItem>
+      <MenuItem value="8">미용/여행/숙박/음식/경비/청소직</MenuItem>
+      <MenuItem value="9">영업/판매/운전/운송직</MenuItem>
+      <MenuItem value="10">건설/채굴직</MenuItem>
+      <MenuItem value="11">설치/정비/생산직</MenuItem>
+      <MenuItem value="12">농림어업직 </MenuItem>
     </MenuBar>
     </MenuBarWrapper>
   );
@@ -137,6 +152,7 @@ function Container1() {
         희망직종
       </Texts>
       <Selectbar />
+      <Writeinput />
     </Container>
   );
 }
