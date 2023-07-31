9e1d10dbc105fc099762433c6253f5ae38c3717a
jiss00 <ksksksk729@gmail.com>
2023-07-31 19:00:29 +0900

    채용정보 취업정보 기능 구현 끝

--- src/information/employment.js
+++ src/information/employment.js
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom'
 import Footer from './footerBar';
 import Top from './Top';
 import '../css/information.css';
+import axios from 'axios';
 
 
 
@@ -16,11 +17,32 @@ function Employment() {
     window.location.href = 'http://localhost:3000'
   }
   const navigate = useNavigate();
+  const [dataList, setDataList] = useState([]); // API 결과값들을 저장할 배열
+  const [page,setPage] = useState('1');
+  console.log(page);
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const url = 'http://arthurcha.shop:3000/app/jobEdu';
+          const response = await axios.get(url,{
+            params: { page: page }, // 동적으로 변경되는 검색어
+          });
+          setDataList(response.data.result);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+      
+      fetchData();
+    }, [page]); // 빈 배열을 넣어 마운트될 때 한 번만 호출하도록 설정
+    console.log(dataList);
 
   return (
     <div>
       <Top route={back} text='취업지원'></Top>
-      <div className="main1">
+      {dataList.length === 0 ? (
+        <></>) : 
+      (<div className="main1">
         <div className="margin"></div>
         <div className="sort">
           <Sort text='마감날짜' ></Sort>
@@ -28,19 +50,19 @@ function Employment() {
           <div></div>
           <Search></Search>
         </div>
-        <Recruiting onClick={() => navigate(`/jopdetail`)} text='시니어 바리스타 자격증 과정'></Recruiting>
-        <Recruiting text='플라워리스트 자격증 과정'></Recruiting>
-        <Recruiting text='요양도우미 자격증 과정'></Recruiting>
-        <Recruiting text='키오스크 사용법 교육'></Recruiting>
-        <Recruiting text='인터넷 사용 교육'></Recruiting>
-        <Recruiting text='휴대폰 사용 교육'></Recruiting>
-        <Complete text='시니어 바리스타 자격증 과정'></Complete>
-        <Complete text='플라워리스트 자격증 과정'></Complete>
-        <Complete text='요양도우미 자격증 과정'></Complete>
-        <Complete text='인터넷 사용 교육'></Complete>
-        <Complete text='휴대폰 사용 교육'></Complete>
-        <Footer></Footer>
-      </div>
+        {dataList[0].active_status === 1 ? <Recruiting text= {dataList[0].title}></Recruiting> : <Complete text= {dataList[0].title}></Complete>}
+        {dataList[1].active_status === 1 ? <Recruiting text= {dataList[1].title}></Recruiting> : <Complete text= {dataList[1].title}></Complete>}
+        {dataList[1].active_status === 1 ? <Recruiting text= {dataList[1].title}></Recruiting> : <Complete text= {dataList[1].title}></Complete>}
+        {dataList[2].active_status === 1 ? <Recruiting text= {dataList[2].title}></Recruiting> : <Complete text= {dataList[2].title}></Complete>}
+        {dataList[4].active_status === 1 ? <Recruiting text= {dataList[4].title}></Recruiting> : <Complete text= {dataList[4].title}></Complete>}
+        {dataList[5].active_status === 1 ? <Recruiting text= {dataList[5].title}></Recruiting> : <Complete text= {dataList[5].title}></Complete>}
+        {dataList[6].active_status === 1 ? <Recruiting text= {dataList[6].title}></Recruiting> : <Complete text= {dataList[6].title}></Complete>}
+        {dataList[7].active_status === 1 ? <Recruiting text= {dataList[7].title}></Recruiting> : <Complete text= {dataList[7].title}></Complete>}
+        {dataList[8].active_status === 1 ? <Recruiting text= {dataList[8].title}></Recruiting> : <Complete text= {dataList[8].title}></Complete>}
+        {dataList[9].active_status === 1 ? <Recruiting text= {dataList[9].title}></Recruiting> : <Complete text= {dataList[9].title}></Complete>}
+        {/*dataList[10].active_status === 1 ? <Recruiting text= {dataList[10].title}></Recruiting> : <Complete text= {dataList[10].title}></Complete>*/}
+        <Footer page={page} setPage = {setPage}></Footer>
+      </div>)}
     </div>
   )
 }
