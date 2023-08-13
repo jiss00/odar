c3673298f7ceb4c315a87da638de2184932462c8
Jiny <moc24@naver.com>
2023-08-13 21:32:25 +0900

    지니: 비밀번호찾기 구현 최종완성, 이메일에서 인증코드 받아옴
    지니: 비밀번호찾기 구현 최종완성, 이메일에서 인증코드 받아옴

--- src/components/FindingPass.js
+++ src/components/FindingPass.js
@@ -17,14 +17,84 @@ function FindingPass(props){
   const [modal_text, set_modal_text] = useState('아이디 인증이 완료되었습니다!');
   // const [time, setTime] = useState(180); // 남은 시간 (단위: 초)
 
+  
+
   const [validPassword, setValidPassword] = useState(false);
   const [validPasswordCheck, setValidPasswordCheck] = useState(false);
   
   // 정규식 확인 ---------------------------------
   // const { onChange, ...otherProps } = props; // props에서 onChange를 추출하여 나머지 props를 otherProps로 받음
 
+  // 유효성검사
   const [validEmail, setValidEmail] = useState(false);
+
+  // di 인증 클릭 --------------------------//
+  const [isCheckModal, setIsCheckModal] = useState(false);
+
+  // --------------인증번호 -----------------//
+  // 인증번호
+  const [certification_number_state, set_certification_number] = useState('');
+
+  // 인증번호 코드 받기
+  const [certification_code , set_certification_code]=useState('');
+
+  // (작은) 인증번호 input 6자리 활성화
+  const [btn_yes, set_btn_yes] = useState(false);
+  // ---------------------------------------------//
+
+  // 인증 후 비밀번호, 새 비밀번호변경부분 보이게하는 속성 //
+  // 처음엔 숨긴다.
+  const [body_pass_visibility, set_body_pass_visibility] = useState("hidden");
+
+
+  // --------------------------인증번호--------------------------------------//
+
+// 인증번호 입력 받기
+  const saveCertificationNumber = event => {
+    set_certification_number(event.target.value);
+  }
+  // 인증번호 : 6자리 누르면  버튼 활성화시키기
+  const isBtnYes = () =>{
+    if (certification_number_state.length ===6){
+      set_btn_yes(true);
+    }else{
+      set_btn_yes(false);
+    }
     
+  }
+
+  useEffect(()=>{
+    isBtnYes();
+    // console.log(certification_number_state);
+    
+  } ,[certification_number_state]);
+
+  // (작은)'완료 버튼 클릭 시'
+  const BtnSuccess = () => {
+    // console.log("btnSuccess");
+    
+    // 인증번호와 내가 입력한게 같다면, 인증성공
+    if (certification_code === certification_number_state) {
+      // console.log("인증성공!");
+      setModal(true);
+      set_modal_text(`인증이 완료되었습니다!🎉\n 새 비밀번호를 입력해주세요.`);
+      // console.log("제일 밑 완료활성화, 인증번호 맞음/  현재 state %d", btn_all_state);
+      // 비밀번호, 새 비밀번호 보이게 하기
+      set_body_pass_visibility("visible"); 
+    }
+    else{
+      setModal(true);
+      // console.log("인증실패!");
+      set_modal_text('유효하지 않은 인증번호입니다.');
+      
+      
+
+    }
+  }
+
+// -------------------------------------------------------------------//
+
+
   // 아이디 이메일 정규식 확인
   const validateEmail = () => {
     const regex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
@@ -49,7 +119,6 @@ function FindingPass(props){
 
   const [password, setPassword] = useState("");
   const [passwordcheck, setPasswordCheck] = useState("");
-  console.log(password);
 
   const handlePasswordChange = ({ password, isValidPassword }) => {
     setValidPassword(isValidPassword);
@@ -73,16 +142,14 @@ function FindingPass(props){
   
   const Modal = function(){
     return(
-      <div id = "modal_success_pass">
+      <pre id = "modal_success_pass">
         {modal_text}
-      </div>
+      </pre>
     );
   }
 
 
   // 네이게이션
-     // 모달-------------------------------------------//
-
   // 제일 밑 완료 버튼 클릭시, 로그인 화면으로 이동, 서버에 업데이트된 비밀번호 전송
   let navigate = useNavigate(); //라우팅 객체 만들기
   const goLogin = () => {
@@ -106,13 +173,16 @@ function FindingPass(props){
             // console.log('200번대 성공');
             setValidEmail(true);//인증완료시 버튼 33%채워짐.
             setModal(true); //모달 보이게하기
-            set_modal_text('아이디 인증이 완료되었습니다!');
+            setIsCheckModal(true); //인증보내기 성공으로 상태 변경.!!
+            // set_certification_code(response.data.result.code);
+            set_modal_text('인증번호가 발송되었습니다. \n이메일을 확인해주세요.');
           }
           else{
           console.log('▶[오류] 코드:'+response.data.code+'\n'+response.data.message);
           setValidEmail(false);//인증완료시 버튼 33%채워짐.
           setModal(true); //모달 보이게하기
           set_modal_text('아이디를 다시 확인해주세요.');
+          setIsCheckModal(false); // 인증번호보내기 실패
           }
         }
         else{
@@ -120,6 +190,7 @@ function FindingPass(props){
           setValidEmail(false);//인증완료시 버튼 33%채워짐.
           setModal(true); //모달 보이게하기
           set_modal_text('아이디를 다시 확인해주세요.');
+          setIsCheckModal(false); // 인증번호보내기 실패
         }
         // setJopDetail(response.recruitment, response.title, response.money, response.time, response.introduction);
     } )
@@ -128,11 +199,16 @@ function FindingPass(props){
       setValidEmail(false);//인증완료시 버튼 33%채워짐.
       setModal(true); //모달 보이게하기
       set_modal_text('아이디를 다시 확인해주세요.');
+      setIsCheckModal(false); // 인증번호보내기 실패
         // console.log(); // 에러 출력
     });
     // 백엔드에서 받아오기 전이므로, 가상의 데이터로 예시 작성
     
-}
+  }
+
+  useEffect(() => {
+    // console.log('현재코드: ' + certification_code);
+  },[certification_code]);
 
   // --------------비밀번호 변경api-----------------//
   // --------------비밀번호 변경api-----------------//
@@ -146,16 +222,17 @@ function FindingPass(props){
 
     axios.patch(url, data)
       .then((response) => {
-        console.log(response);
+        // console.log(response);
         if (response.data.isSuccess){
+          
           alert("비밀번호가 변경되었습니다! 로그인 화면으로 이동합니다.");
-          console.log(response.data.message);
+          // console.log(response.data.message);
           goLogin();
           
         }
         else{
           alert("비밀번호 변경에 실패하였습니다.");
-          console.log(response.data.message);
+          // console.log(response.data.message);
         }
 
       })
@@ -166,7 +243,51 @@ function FindingPass(props){
 
   }
 
-  useEffect(()=> { }, [email]);
+
+
+
+  const getSeconds = (time) => {
+    const seconds = Number(time % 60);
+    if (seconds < 10) {
+      return "0" + String(seconds);
+    } else {
+      return String(seconds);
+    }
+  }
+  // Timer 컴포넌트에서 사용할 상태
+  const [time, setTime] = useState(180); // 남은 시간 (단위: 초)
+  const Timer = ({ time }) => {
+
+    // Timer 컴포넌트에서 사용할 함수
+    const handleTimerEnd = () => {
+      console.log("타이머종료");
+      // 타이머가 종료되었을 때 필요한 처리를 여기서 수행하십시오.
+      // 예를 들면, 인증번호 만료 처리 등
+      // 시간이 종료됐을 때 실행될 로직을 추가해주세요.
+    };
+    useEffect(() => {
+      setModal(true);
+      const timer = setInterval(() => {
+        setTime((prev) => prev - 1);
+      }, 1000);
+      if (time === 0) {
+        clearInterval(timer);
+        handleTimerEnd();
+        alert('시간경과');
+        setModal(false);
+      }
+      return () => clearInterval(timer);
+    }, [time]);
+    return (
+      <div>
+        <div className='timer'>
+          <span>{parseInt(time / 60)}</span>
+          <span> : </span>
+          <span>{getSeconds(time)}</span>
+        </div>
+      </div>
+    );
+  }
 
 
   return(
@@ -184,13 +305,20 @@ function FindingPass(props){
         <h3 className="id_pass">아이디</h3>
         <section className='input_section1_pass'>
           <input className="id_input_pass" onChange={handleInputChange} type="text" id="" placeholder="oooo@oooo" ></input>
-          <button className= {validEmail === false ? "btn_all_pass" : "btn_all_pass_yes"} disabled ={!validEmail} type="submit"  onClick={() => { checkModal(); }} >인증</button>
+          <button className= {validEmail === false ? "btn_all_pass" : "btn_all_pass_yes"} disabled ={!validEmail} type="submit"  onClick={() => { set_modal_text('id가 유효한지 확인중입니다...'); setModal(true); checkModal(); }} >인증</button>
+        </section>
+        {/* {}를 쓰면 js 코드 쓸 수 있다. */}
+        {/* 인증버튼. */}
+        <section className='input_section2'>
+          <input onChange={saveCertificationNumber} className="id_input_pass" type="text"  placeholder="000000" maxLength={6} ></input>
+          <span></span>
+          {modal === true ? <Timer time={time}></Timer> : <></>}
+          <button disabled={ !btn_yes && !isCheckModal} onClick={() => {BtnSuccess(); } } className={ btn_yes && isCheckModal? 'btn_all_pass_yes' : 'btn_all_pass'} type="submit">완료</button>
         </section>
         {modal === true ? <Modal></Modal> : <></>}
-        {/* {}를 쓰면 js 코드 쓸 수 있다. */}
       </div>
 
-      <div className = "body_pass_pass">
+      <div className = "body_pass_pass" style={{visibility: `${body_pass_visibility}`}}>
         <div className="findingpass__pw__box"><Text width="110px">새 비밀번호</Text></div>
           <FindingPass_Input_pw type="password" placeholder=" 비밀번호를 입력해주세요." onChange={handlePasswordChange} />
 
@@ -200,7 +328,7 @@ function FindingPass(props){
 
     
 
-      <button
+      <button 
           disabled={validEmail && validPassword && validPasswordCheck ? false : true}
           className="btn_all_pass"
           id="btn_success_pass"
