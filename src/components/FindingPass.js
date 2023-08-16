d66d56fb60568f279b75cc6c463815b7aee8e1f4
Jiny <moc24@naver.com>
2023-08-17 00:59:33 +0900

    지니 : 아이디,비밀번호 ui 진짜수정됨
    지니 : 아이디,비밀번호 ui 진짜수정됨

--- src/components/FindingPass.js
+++ src/components/FindingPass.js
@@ -285,7 +285,7 @@ function FindingPass(props){
     }, [time]);
     return (
       <div>
-        <div className='timer'>
+        <div className='timer_pass'>
           <span>{parseInt(time / 60)}</span>
           <span> : </span>
           <span>{getSeconds(time)}</span>
@@ -314,12 +314,9 @@ function FindingPass(props){
             setTime(180); set_body_pass_visibility("hidden"); set_modal_text('id가 유효한지 확인중입니다...'); setModal(true); checkModal(); }} >인증</button>
         </section>
         {/* {}를 쓰면 js 코드 쓸 수 있다. */}
-
-        {/* 인증버튼.  */}
+        {/* 인증버튼. */}
         <section className='input_section2_pass'>
-
           <input onChange={saveCertificationNumber} className="id_input_pass" type="text"  placeholder="000000" maxLength={6} ></input>
-          <span></span>
           {modal === true ? <Timer time={time}></Timer> : <></>}
           <button disabled={ !btn_yes || !isCheckModal} onClick={() => {BtnSuccess(); } } className={ btn_yes && isCheckModal? 'btn_all_pass_yes' : 'btn_all_pass'} type="submit">완료</button>
         </section>
@@ -335,6 +332,7 @@ function FindingPass(props){
       </div>
 
     
+
       <button 
           disabled={validEmail && validPassword && validPasswordCheck ? false : true}
           className="btn_all_pass"
