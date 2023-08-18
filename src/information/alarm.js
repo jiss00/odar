5e43f93b52e8746b41852c8efde9f4bf2b540c4d
jiss00 <ksksksk729@gmail.com>
2023-08-18 12:57:22 +0900

    ui수정

--- src/information/alarm.js
+++ src/information/alarm.js
@@ -41,7 +41,7 @@ function Alarm() {
   }, []);
   console.log(alarmList);
   return (
-    <div className='main_detail'>
+    <div className='main_alarm'>
       <Div>
         <Text text='알림함'></Text>
         <svg className='img_alarm' onClick={onclick} viewBox="0 0 35 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 30V19.4118H21V30H29.75V15.8824H35L17.5 0L0 15.8824H5.25V30H14Z" fill="#5B8E31" /></svg>
